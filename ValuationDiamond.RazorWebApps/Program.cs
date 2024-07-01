8b970a2feb9d47d795f2803d36c651ef9f176ac5
Vu Hoang Duy Khanh <khanh911234@gmail.com>
2024-07-01 23:28:30 +0700

    Khanh/Fix

--- ValuationDiamond.RazorWebApps/Program.cs
+++ ValuationDiamond.RazorWebApps/Program.cs
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 using Microsoft.EntityFrameworkCore;
 using ValuationDiamond.Business;
 using ValuationDiamond.Data.Models;
-=======
 using ValuationDiamond.Business;
->>>>>>> 48ca876242f680a9a18046acb81f17f6fe964638
 
 namespace ValuationDiamond.RazorWebApps
 {
@@ -16,16 +13,13 @@ namespace ValuationDiamond.RazorWebApps
 
             // Add services to the container.
             builder.Services.AddRazorPages();
-<<<<<<< HEAD
             builder.Services.AddScoped<IOrderBusiness, OrderBusiness>();
             builder.Services.AddScoped<ICustomerBusiness, CustomerBusiness>();
             //builder.Services.AddDbContext<Net1710_221_2_ValuationDiamondContext>(options =>
                 //options.UseSqlServer(builder.Configuration.GetConnectionString("Net1710_221_2_ValuationDiamondContext") ?? throw new InvalidOperationException("Connection string 'Net1710_221_2_ValuationDiamondContext' not found.")));
 
-=======
             builder.Services.AddScoped<ValuationCertificateBusiness>();
                 
->>>>>>> 48ca876242f680a9a18046acb81f17f6fe964638
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
